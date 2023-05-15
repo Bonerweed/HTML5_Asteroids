70d007b86794373a8864eb72f900b0e071c3f427
Bonerweed <garenmcswagballing@gmail.com>
2023-05-15 20:03:40 +0300

    fix: update babylon, p5 now actually draws a thing

--- engines/p5.js
+++ engines/p5.js
@@ -1,4 +1,3 @@
-//import * as p5api from "./p5api.js";
 export default class P5Test {
   constructor(div, resourceLocations, sprites, firstMax) {
     //this.app = new PIXI.Application();
@@ -20,36 +19,48 @@ export default class P5Test {
       script.src = "./engines/resources/p5.min.js";
       document.body.appendChild(script);
     });
-    //script.onload = scriptLoaded;
-    /*const apipromise = new Promise((resolve, reject) => {
-      const api = document.createElement("script");
-      //api.type = "text/javascript";
-      api.onload = () => {resolve()};
-      api.onerror = () => {reject};
-      api.src = "./engines/p5api.js";
-      document.body.appendChild(api);
-  });*/
     await promise;
     //await apipromise;
-    console.log("promise over");
+    const otherPromise = new Promise((resolve, reject)=>{
+      console.log("promise over");
     const seed = (sketch) => {
-      let x = 800;
-      let y = 600;
+      let x = div.offsetWidth;
+      let y = div.offsetHeight;
+      sketch.preload = () => {
+        sketch.spaceShip = sketch.loadImage(this.shipImage);
+        sketch.spaceRock = sketch.loadImage(this.rockImage);
+      };
+      sketch.partList = this.rawSprites;
       sketch.setup = () => {
         sketch.createCanvas(x, y);
+        sketch.noLoop();
+        
       }
-
+      //console.log(sketch.spaceShip, this.rawSprites[0].posX, this.rawSprites[0].posY);
+      //sketch.ship = sketch.image(sketch.spaceShip, this.rawSprites[0].posX, this.rawSprites[0].posY);
       sketch.draw = () => {
         sketch.background(0);
         sketch.fill(255);
-        sketch.rect(100,100,50,50);
+        //sketch.rect(100,100,50,50);
+        sketch.ship = sketch.image(sketch.spaceShip, this.rawSprites[0].posX, this.rawSprites[0].posY);
       }
     };
     console.log(div.offsetHeight, div.offsetWidth);
     this.p5Instance = new p5(seed, div);
+    const shipSprite = this.rawSprites[0];
+    });
+    await otherPromise;
+    //this.ship = this.p5Instance.game.createSprite(shipSprite.posX, shipSprite.posY, 24, 24);
+    //this.ship = new this.p5Instance.Sprite(shipSprite.posX, shipSprite.posY, 24, 24);
+    //this.ship.addImage(this.p5Instance.game.loadImage(this.shipImage))
     //check successs here
   };
   drawFrame(frame, max, inputs) {
+    if (!this.p5Instance) {
+      console.log("not ready yet");
+      return;
+    }
+    this.p5Instance.redraw()
   };
   checkCollision() {};
   destroy() {};
